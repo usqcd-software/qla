4494e159b4b190e4d34f0a63ff156728be6fdcde
osborn <>
2011-01-22 20:51:57 +0000

    fixed recently introduced bugs in some SSE routines

--- lib/optimized/sse/include/QLA_F3_D_vpeq_spproj_M_times_pD_sse.c
+++ lib/optimized/sse/include/QLA_F3_D_vpeq_spproj_M_times_pD_sse.c
@@ -5,11 +5,13 @@
       n = 1;
       nn--;
 #include <QLA_F3_D_vpeq_spproj_M_times_pD_1_sse.c>
+      r += n; a += n; b += n;
     }
     n = 2*(nn/2);
     nn -= n;
 #include <QLA_F3_D_vpeq_spproj_M_times_pD_2_sse.c>
     if(nn) {
+      r += n; a += n; b += n;
       n = nn;
 #include <QLA_F3_D_vpeq_spproj_M_times_pD_1_sse.c>
     }
--- lib/optimized/sse/include/QLA_F3_D_vpeq_sprecon_M_times_pH_sse.c
+++ lib/optimized/sse/include/QLA_F3_D_vpeq_sprecon_M_times_pH_sse.c
@@ -5,11 +5,13 @@
       n = 1;
       nn--;
 #include <QLA_F3_D_vpeq_sprecon_M_times_pH_1_sse.c>
+      r += n; a += n; b += n;
     }
     n = 2*(nn/2);
     nn -= n;
 #include <QLA_F3_D_vpeq_sprecon_M_times_pH_2_sse.c>
     if(nn) {
+      r += n; a += n; b += n;
       n = nn;
 #include <QLA_F3_D_vpeq_sprecon_M_times_pH_1_sse.c>
     }
